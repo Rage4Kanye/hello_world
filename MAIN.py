9c07781f9418af197070ddc6230032c7cbfe3f67
rage4kanye <dkratzk@gmail.com>
2018-05-30 22:23:05 -0500

    added color to label

--- MAIN.py
+++ MAIN.py
@@ -2,7 +2,7 @@ import tkinter as tk
 
 root = tk.Tk()
 
-labe1 = tk.Label(root, text='Hello_World')
+labe1 = tk.Label(root, text='Hello_World', fg='red', bg='blue')
 labe1.pack()
 
 button = tk.Button(root, text='Close', command=root.quit)
